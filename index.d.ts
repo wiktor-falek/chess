ddefdd1b0b53ba70b486e6834f073fc8cee74b9e
wiktor-falek <falekwiktor@gmail.com>
2023-07-01 15:02:12 +0200

    import pieces from index

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
-import Pawn from "./src/logic/pieces/pawn";
-import Knight from "./src/logic/pieces/knight";
-import Bishop from "./src/logic/pieces/bishop";
-import Rook from "./src/logic/pieces/rook";
-import Queen from "./src/logic/pieces/queen";
-import King from "./src/logic/pieces/king";
+import {
+  Pawn,
+  Knight,
+  Bishop,
+  Rook,
+  Queen,
+  King,
+} from "./src/logic/pieces/index";
 
 export type Color = "white" | "black";
 export type Piece = Pawn | Knight | Bishop | Rook | Queen | King;
